2aa90add8c87b7b5afe4473a6c887fe69930e3b7
Leulteferii <148948575+Leulteferii@users.noreply.github.com>
2023-11-17 20:00:19 +0300

    Add files via upload

---
+++ time.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int fs;
+cout<<"File size(in mega bytes): ";
+cin>>fs;
+
+{
+        cout<<"The time it takes is "<<fs / (float) 0.0009155<<" seconds\n";
+
+}
+
+
+
+
+    return 0;
+
+}
